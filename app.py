0efce4a5adb01341c0c8ec223cca6f05efe3072c
admin <admin@huynqs-MacBook-Pro-259.local>
2018-08-22 17:54:03 +0700

    .

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from google.cloud import firestore
+
+app = Flask(__name__)
+api = Api(app)
+
+class DeadlineListRes(Resource):
+    def get(self):
+        return {
+            'response_type': 'in_channel',
+            'text': 'Deadline is comming',
+        }
+
+api.add_resource(DeadlineListRes, '/slack/deadlines')
+
+if __name__ == '__main__':
+    app.run(debug=True)
